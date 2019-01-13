ce50c1b9de39099d11fcf2872912ff220c2553b6
Supergecki <46622920+Supergecki@users.noreply.github.com>
2019-01-13 18:01:19 +0100

    Add files via upload

--- serversocket_german.py
+++ serversocket_german.py
@@ -1,64 +1,62 @@
-# Program to receive values from client and write it into database.
+# Programm, um die gemessenen Werte vom Sensor Client zu empfangen und sie in die Datenbank zu schreiben.
+# Gibt außerdem Anweisungen für Display und Actor Clients.
 
-# Imports all modules needed.
-from socket import socket, AF_INET, SOCK_STREAM, gethostbyname, gethostname # used for socketing
-from threading import Thread                                                # used for multithreaded application
-import atexit                                                               # used to clean server at the end
-import sqlite3                                                              # used for database access
-from time import strftime, sleep                                            # used to paste time into database
-import sys                                                                  # used to end program
+# Importiert alle benötigten Module.
+from socket import socket, AF_INET, SOCK_STREAM, gethostbyname, gethostname # für Socket-Verbindungen
+from threading import Thread                                                # für Multithreading (sodass mit mehreren Clients gleichzeitig kommuniziert werden kann)
+import atexit                                                               # um den Server am Ende des Programms zu beenden
+import sqlite3                                                              # für den Zugriff auf die Datenbank
+from time import strftime, sleep                                            # für aktuelle Zeitabfrage und Wartezeiten
+import sys                                                                  # um das Programm zu beenden
 
-# Sets some constants.
-HOST = gethostbyname(gethostname()) # own IP address
-PORT = 33000                        # port to set up serversocket
-BUFSIZ = 1024                       # maximum amount of data to be received once
-ADDR = ('', PORT)                   # address to set up serversocket
-AIRVALUE   = 870                    # minimum humidity value -->   0%
-WATERVALUE = 450                    # maximum humidity value --> 100%
+# Definiert einige Konstanten.
+HOST = gethostbyname(gethostname()) # eigene IP Adresse
+PORT = 33000                        # Port, auf dem der Server gestartet wird
+BUFSIZ = 1024                       # maximale Datenmenge, die auf einmal empfangen werden kann
+ADDR = ('', PORT)                   # Adresse, auf der der Server gestartet wird ('' steht dafür, dass mehrere Geräte auf den Server zugreifen können)
+AIRVALUE   = 870                    # minimaler Feuchtigkeitswert -->   0%
+WATERVALUE = 450                    # maximaler Feuchtigkeitswert --> 100%
 
-# Creates the IP and client dictionaries.
-# IP dict: translates IP address to device name
+# Erstellt das IP Dictionary (spezieller Datentyp in Python).
+# Übersetzt eine IP Adresse in einen Gerätenamen. 
 ip_dict = {
 '192.168.188.111':'Sensor 1',
 '192.168.188.112':'Sensor 2',
 '192.168.188.113':'Actor 1',
 '192.168.188.115':'Display'
 }
-
-# Client dict: translates device name to IP address
-client_dict = {}
-for key in ip_dict:
-	client_dict[ip_dict[key]] = key
 	
-last_msg_s1 = '' # stores the last message received (Sensor 1)
-last_msg_s2 = '' # stores the last message received (Sensor 2)
-new_data = False # stores if the values were sent to the display
-new_data2 = False # stores if the values were sent to the actors
+last_msg_s1 = '' # speichert den letzten Wert von Sensor Client 1
+last_msg_s2 = '' # speichert den letzten Wert von Sensor Client 2
+new_data = False # speichert, ob die Daten bereits an den Display Client gesendet wurden
+new_data2 = False # speichert, ob die Daten bereits an die Actor Clients gesendet wurden
 
-humidity_standard = 50 # the standard humidity value, actors will put water on it if it's below
+humidity_standard = 50 # Standardfeuchtigkeitswert (in %), die Actor Clients werden Wasser zuführen, wenn der echte Wert niedriger ist
+# Dies ist nur ein Beispielwert. Der richtige Wert muss je nach Erd- und Pflanzentyp angepasst werden.
 
-# Connection accepting thread, accepts all incoming connections (starts a handle_client thread).
+# Dieser Thread baut die Verbindungen zu allen Clients auf, die versuchen, sich mit dem Server zu verbinden.
+# Für jeden dieser wird ein Thread gestartet, über den Server und Client miteinander interagieren.
 def accept_incoming_connections():
 	while True:
 		client, client_address = SERVER.accept()
 		HANDLE_THREAD = Thread(target=handle_client, args=(client, client_address))
 		HANDLE_THREAD.start()
 
-# Handles the connection to a single client.	
+# Dieser Thread steuert die Interaktion zwischen Server und Client.	
 def handle_client(client, client_address):
 	global last_msg_s1, last_msg_s2, new_data, new_data2
 	try:
-		if ip_dict[client_address[0]].startswith('Sensor'): # check if device is a sensor client
-			conn = sqlite3.connect('/var/www/html/database/sensordata.db') # connects to database
-			c = conn.cursor()                                              # and enables sqlite commands
+		if ip_dict[client_address[0]].startswith('Sensor'): # überprüft, ob das Gerät ein Sensor Client ist
+			conn = sqlite3.connect('/var/www/html/database/sensordata.db') # öffnet die Datenbank für den Schreibzugriff
+			c = conn.cursor()                                              # und ermöglicht SQLite-Befehle
 			while True:
-				raw_msg = client.recv(BUFSIZ)                              # receive message from client
+				raw_msg = client.recv(BUFSIZ)                              # empfängt unformatierte Messwerte vom Sensor Client
 				if raw_msg:
-					raw_msg = raw_msg.decode("utf8")                       # decode message from bytes to string
-					parts = [raw_msg[i:i+3] for i in range(0, len(raw_msg), 3)]
+					raw_msg = raw_msg.decode("utf8")                       # decodiert die Messwerte in eine Zeichenkette
+					parts = [raw_msg[i:i+3] for i in range(0, len(raw_msg), 3)] # stellt sicher, dass nur Messwerte aus drei Zeichen gesendet werden
 					for msg1 in parts:
 						msg = msg1
-						msg = round((AIRVALUE - int(msg)) / (AIRVALUE - WATERVALUE) * 100, 2)
+						msg = round((AIRVALUE - int(msg)) / (AIRVALUE - WATERVALUE) * 100, 2) # rechnet den unformatierten Wert in eine Prozentzahl um
 						print(msg)
 						if ip_dict[client_address[0]] == 'Sensor 1':
 							last_msg_s1 = str(msg)
@@ -66,45 +64,45 @@ def handle_client(client, client_address):
 							last_msg_s2 = str(msg)
 						new_data = True
 						new_data2 = True
-						c.execute("INSERT INTO sensorreadings(humidity, date, time, device) values(%s, date('now'), time('now'), \"%s\")" % (msg, ip_dict[client_address[0]])) # inserts data into database
-						conn.commit() # and commits
-		elif ip_dict[client_address[0]] == 'Display': # check if device is the display
+						c.execute("INSERT INTO sensorreadings(humidity, date, time, device) values(%s, date('now'), time('now'), \"%s\")" % (msg, ip_dict[client_address[0]])) # schreibt Daten in die Datenbank
+						conn.commit() # und bestätigt
+		elif ip_dict[client_address[0]] == 'Display': # überprüft, ob das Gerät der Display Client ist
 			while True:
-				client.send(b'%s;%s' % (bytes(last_msg_s1, "utf8"), bytes(last_msg_s2, 'utf8'))) # send values to display
+				client.send(b'%s;%s' % (bytes(last_msg_s1, "utf8"), bytes(last_msg_s2, 'utf8'))) # sendet Werte an den Display Client
 				new_data = False
-				while not new_data: # idling while no new data is received
+				while not new_data: # wartet darauf, dass neue Daten empfangen werden
 					sleep(1)
-		elif ip_dict[client_address[0]].startswith('Actor'): # check if device is an actor client
+		elif ip_dict[client_address[0]].startswith('Actor'): # überprüft, ob das Gerät ein Actor Client ist
 			while True:
-				while not new_data2: # idling while no new data is received
+				while not new_data2: # wartet darauf, dass neue Daten empfangen werden
 					sleep(1)
 				if last_msg_s1:
-					client.send(b'%s(1)' % (b'open_gate' if float(last_msg_s1) < humidity_standard else b'close_gate'))
+					client.send(b'%s(1)' % (b'open_gate' if float(last_msg_s1) < humidity_standard else b'close_gate')) # sendet Befehl an Actor Client 1
 				if last_msg_s2:
-					client.send(b'%s(2)' % (b'open_gate' if float(last_msg_s2) < humidity_standard else b'close_gate'))
+					client.send(b'%s(2)' % (b'open_gate' if float(last_msg_s2) < humidity_standard else b'close_gate')) # sendet Befehl an Actor Client 2
 				new_data2 = False
 	except (ConnectionResetError, OSError) as e:
 		pass
 
-# Cleans the server port for a new program start.		
+# Beendet den Server, damit der Port neu benutzt werden kann.		
 def cleanserver():
 	SERVER.close()
 
-# Sets up the server at the given address.
+# Startet den Server auf der angegebenen Adresse.
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
-# Calls cleanserver() at the end of the program.
+# Ruft die cleanserver()-Funktion am Ende des Programms auf, damit der Server beendet wird.
 atexit.register(cleanserver)
 
-# Opens the server for incoming connections.
+# Öffnet den Server, sodass die Clients darauf zugreifen können.
 try:
 	SERVER.listen(5)
 	ACCEPT_THREAD = Thread(target=accept_incoming_connections)
 	ACCEPT_THREAD.start()
 	ACCEPT_THREAD.join()
 	
-# Program is stopped manually, so clean the server.
+# Wenn das Programm manuell oder unerwartet beendet wird, soll der Server beendet werden.
 except KeyboardInterrupt:
 	cleanserver()
 	sys.exit()
